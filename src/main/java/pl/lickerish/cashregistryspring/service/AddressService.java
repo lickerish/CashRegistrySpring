23faec93b4a4da0b6b3c363e6eca7047a157574e
lickerish <36692568+lickerish@users.noreply.github.com>
2019-09-22 10:29:16 +0200

    Add create method to AddressService

--- src/main/java/pl/lickerish/cashregistryspring/service/AddressService.java
+++ src/main/java/pl/lickerish/cashregistryspring/service/AddressService.java
@@ -2,6 +2,8 @@ package pl.lickerish.cashregistryspring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.lickerish.cashregistryspring.DTO.AddressDTO;
+import pl.lickerish.cashregistryspring.model.Address;
 import pl.lickerish.cashregistryspring.repository.AddressRepository;
 
 @Service
@@ -12,5 +14,9 @@ public class AddressService {
     @Autowired
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
+    }
+
+    public Address create(AddressDTO address) {
+        return addressRepository.save(new Address(address.getId(), address.getStreet(), address.getCity(), address.getZipCode()));
     }
 }
